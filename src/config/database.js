33b4b81d1463e1cb6edb8723b2d6f7c24aa76350
CooolRyan <74342019+CooolRyan@users.noreply.github.com>
2025-05-07 11:10:04 +0900

    ✅test : 암호화 / 복호화 테스트

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/woorepie');
+        await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/sto');
         console.log('MongoDB 연결 성공');
     } catch (error) {
         console.error('MongoDB 연결 실패:', error);
